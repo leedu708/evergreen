f835049548cdf0e3bc6fedc8551e1b663df9381a
Dustin <leedu708@gmail.com>
2016-02-14 16:27:21 -0500

    added more top 3's to overview

--- app/assets/javascripts/controllers/OverviewCtrl.js
+++ app/assets/javascripts/controllers/OverviewCtrl.js
@@ -1,8 +1,9 @@
 evergreen.controller('OverviewCtrl',
-  ['$scope', 'Restangular', 'users', 'sectors', 'collections', 'resources', 'resourceService',
-  function($scope, Restangular, users, sectors, collections, resources, resourceService) {
+  ['$scope', 'Restangular', 'users', 'sectors', 'collections', 'resources', 'resourceService', 'userService',
+  function($scope, Restangular, users, sectors, collections, resources, resourceService, userService) {
 
     $scope.init = function() {
+      userService.setUsers(users);
       resourceService.setResources(resources);
       $scope.setOverviewVars();      
     };
@@ -13,6 +14,8 @@ evergreen.controller('OverviewCtrl',
       $scope.totalCollections = collections.length;
       $scope.totalResources = resources.length;
       $scope.topThree = resourceService.getTopResources(3);
+      $scope.topContributors = userService.getTopContributors(3);
+      $scope.top3Prolific = userService.getMostProlific(3);
     };
 
     $scope.init();
