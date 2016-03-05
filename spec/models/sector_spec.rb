6c1aac8ab6289aead7b0566faf67b3f08723190c
Dustin <leedu708@gmail.com>
2016-03-04 23:45:11 -0500

    updated model tests

--- spec/models/sector_spec.rb
+++ spec/models/sector_spec.rb
@@ -1,5 +1,8 @@
 RSpec.describe Sector, type: :model do
 
   it { should have_many(:categories).with_foreign_key(:sector_id).dependent(:nullify) }
+  it { should have_many(:collections).through(:categories) }
+  it { should have_many(:resources).through(:collections) }
+  it { should have_many(:upvotes).through(:resources) }
 
 end
